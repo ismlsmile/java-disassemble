58d59b20e6ef9876de6461a52f0d30214ffd14db
rbrick <rbrickproductions123@gmail.com>
2018-10-18 13:28:08 -0500

    prefix java versions

--- java.go
+++ java.go
@@ -2,13 +2,13 @@ package disassemble
 
 // constants for java versions
 const (
-	SE11 = 0x37 - iota
-	SE10
-	SE9
-	SE8
-	SE7
-	SE6
-	SE5
+	JavaSE11 = 0x37 - iota
+	JavaSE10
+	JavaSE9
+	JavaSE8
+	JavaSE7
+	JavaSE6
+	JavaSE5
 	JDK14
 	JDK13
 	JDK12
